65d0d8c680e234bf118a17cb191bafca5e49d499
André <andrerrr@outlook.de>
2019-01-08 15:35:03 +0100

    Methoden vs. Functions

---
+++ HalloMethoden/HalloMethoden/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HalloMethoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zeige("Start");
+
+            SagHallo();
+            string msg = "Lecker Schnitzel!";
+            Zeige(msg);
+            Zeige(Verdoppeln(Verdoppeln(Verdoppeln(Verdoppeln(698)))));
+
+            long zahl = GiveMeFive();
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void SagHallo()
+        {
+            Zeige("Hallo");
+        }
+
+        static void Zeige(string txt)
+        {
+            Console.WriteLine($"[{DateTime.Now}]{txt}");
+            Debug.WriteLine($"DEBUG {txt}");
+        }
+
+        static void Zeige(int zahl)
+        {
+            Console.WriteLine($"Zahl: {zahl}");
+        }
+        static void Zeige(int zahl1, int zahl2)
+        {
+            Console.WriteLine($"Zahlen: {zahl1} & {zahl2} ");
+        }
+
+        static long GiveMeFive()
+        {
+            return 5;
+        }
+
+        static int Verdoppeln(int zahl)
+        {
+            return zahl * 2;
+        }
+    }
+}
